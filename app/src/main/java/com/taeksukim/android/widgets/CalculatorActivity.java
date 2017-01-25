3f0457b49a18107a40c5349bb04e551666bd7dd5
TaeksuKim <taeka89@gmail.com>
2017-01-25 10:56:50 +0900

    update

---
+++ app/src/main/java/com/taeksukim/android/widgets/CalculatorActivity.java
@@ -0,0 +1,157 @@
+package com.taeksukim.android.widgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView result;
+    Button btn0;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    Button btn8;
+    Button btn9;
+
+    Button plus1;
+    Button minus1;
+    Button multiply1;
+    Button divide1;
+    Button cancle1;
+    Button run1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Calculator);
+
+        result = (TextView) findViewById(R.id.result);
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+
+        plus1 = (Button) findViewById(R.id.plus1);
+        minus1 = (Button) findViewById(R.id.minus1);
+        multiply1 = (Button) findViewById(R.id.multiply1);
+        divide1 = (Button) findViewById(R.id.divide1);
+        cancle1 = (Button) findViewById(R.id.cancle1);
+        run1 = (Button) findViewById(R.id.run1);
+
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        cancle1.setOnClickListener(this);
+        plus1.setOnClickListener(this);
+        divide1.setOnClickListener(this);
+        minus1.setOnClickListener(this);
+        multiply1.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        switch(view.getId()) {
+
+            case R.id.btn0:
+                result.setText(result.getText() + "0");
+                break;
+
+            case R.id.btn1:
+                result.setText(result.getText() + "1");
+                break;
+
+            case R.id.btn2:
+                result.setText(result.getText() + "2");
+                break;
+
+            case R.id.btn3:
+                result.setText(result.getText() + "3");
+                break;
+
+            case R.id.btn4:
+                result.setText(result.getText() + "4");
+                break;
+
+            case R.id.btn5:
+                result.setText(result.getText() + "5");
+                break;
+
+            case R.id.btn6:
+                result.setText(result.getText() + "6");
+                break;
+
+            case R.id.btn7:
+                result.setText(result.getText() + "7");
+                break;
+
+            case R.id.btn8:
+                result.setText(result.getText() + "8");
+                break;
+
+            case R.id.btn9:
+                result.setText(result.getText() + "9");
+                break;
+
+            case R.id.cancle1:
+                result.setText("result : ");
+                break;
+
+            case R.id.run1:
+                //result.setText();
+
+
+
+
+
+
+                break;
+
+            case R.id.plus1:
+                result.setText(result.getText() + "+");
+                break;
+
+            case R.id.minus1:
+                result.setText(result.getText() + "-");
+                break;
+
+            case R.id.multiply1:
+                result.setText(result.getText() + "x");
+                break;
+
+            case R.id.divide1:
+                result.setText(result.getText() + "/");
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/taeksukim/android/widgets/MainActivity.java
@@ -0,0 +1,49 @@
+package com.taeksukim.android.widgets;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //1. 위젝을 정의한다.
+    Button btn;
+    Button cal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        //2. 정의된 위젯 변수에 xml의 위젝 id를 가져와서 담아준다.
+        btn = (Button) findViewById(R.id.btnGrid);
+        //3. 변수(위젯)을 리스너에 달아준다.
+        btn.setOnClickListener(this);
+
+        cal = (Button) findViewById(R.id.cal);
+        cal.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()) {
+            case R.id.btnGrid:
+                //안드로이드 메이저 컴포넌트를 로드하기 위해서는
+                // intent를 통해서 로드할 컴포넌트를 지정해야한다.
+                intent = new Intent(this, GridActivity.class);
+                                            //^ 대상이되는 컴포넌트
+                startActivity(intent);
+                break;
+
+            case R.id.cal:
+                intent = new Intent(this, CalculatorActivity.class);
+
+                startActivity(intent);
+                break;
+        }
+    }
+}
